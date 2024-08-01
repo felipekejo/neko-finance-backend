b0f774df3eb0420088ef2a4b8789ad1d3041af52
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-08-01 16:07:08 +0930

    add jwt

---
+++ src/infra/http/controller/create-user.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { PrismaService } from 'src/infra/database/prisma/prisma.service'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+@Controller('users')
+export class CreateUserController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User already exists')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+
+    return user
+  }
+}
