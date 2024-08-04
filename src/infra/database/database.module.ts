6dc6193b22e449d554c32afec9482dd622a5498f
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-08-05 00:41:40 +0930

    add at database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaAccountsRepository } from './prisma/repositories/prisma-accounts-repository'
+import { PrismaBudgetsRepository } from './prisma/repositories/prisma-budgets-repository'
+import { PrismaCategoryRepository } from './prisma/repositories/prisma-category-repository'
+import { PrismaSubcategoriesRepository } from './prisma/repositories/prisma-subcategory-repository'
+import { PrismaTransactionsRepository } from './prisma/repositories/prisma-transactions-repository'
+import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository'
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    PrismaAccountsRepository,
+    PrismaBudgetsRepository,
+    PrismaCategoryRepository,
+    PrismaUsersRepository,
+    PrismaTransactionsRepository,
+    PrismaSubcategoriesRepository,
+  ],
+  exports: [PrismaService],
 })
 export class DatabaseModule {}
