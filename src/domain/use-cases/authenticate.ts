341243eceebf914b279e32dd1f56651c155a4a19
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-09-26 16:11:20 +0930

    create user and authenticate

--- src/domain/use-cases/authenticate.ts
+++ src/domain/use-cases/authenticate.ts
@@ -1,16 +1,14 @@
 import { Either, left, right } from '@/core/either'
 import { Injectable } from '@nestjs/common'
-import { HashGenerator } from '../cryptography/hash-generator'
-import { User } from '../entities/user'
+import { Encrypter } from '../cryptography/encrypter'
+import { HashComparer } from '../cryptography/hash-comparer'
 import { UsersRepository } from '../repositories/user-repository'
 import { UserAlreadyExistError } from './errors/user-already-exist-error'
 
 type UserRole = 'ADMIN' | 'CLIENT'
 interface AuthenticateUserUseCaseRequest {
-
   email: string
   password: string
-
 }
 
 type AuthenticateUserUseCaseResponse = Either<
@@ -24,28 +22,19 @@ type AuthenticateUserUseCaseResponse = Either<
 export class AuthenticateUserUseCase {
   constructor(
     private usersRepository: UsersRepository,
-    private hashGenerator: HashGenerator,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
   ) {}
 
   async execute({
-    name,
     email,
     password,
-    role,
   }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
-    const userWithSameEmail = await this.usersRepository.findByEmail(email)
-    if (userWithSameEmail) {
+    const user = await this.usersRepository.findByEmail(email)
+    if (!user) {
       return left(new UserAlreadyExistError(email))
     }
 
-    const hashedPassword = await this.hashGenerator.hash(password)
-    const user = User.({
-      name,
-      email,
-      password: hashedPassword,
-      role,
-    })
-    await this.usersRepository.Authenticate(user)
     return right({ user })
   }
 }
