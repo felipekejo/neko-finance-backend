398f5b9d22c09fc16f3a10b723a11a65079292c3
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-10-02 11:22:38 +0930

    add auth

--- src/domain/use-cases/authenticate.ts
+++ src/domain/use-cases/authenticate.ts
@@ -3,16 +3,15 @@ import { Injectable } from '@nestjs/common'
 import { Encrypter } from '../cryptography/encrypter'
 import { HashComparer } from '../cryptography/hash-comparer'
 import { UsersRepository } from '../repositories/user-repository'
-import { UserAlreadyExistError } from './errors/user-already-exist-error'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
 
-type UserRole = 'ADMIN' | 'CLIENT'
 interface AuthenticateUserUseCaseRequest {
   email: string
   password: string
 }
 
 type AuthenticateUserUseCaseResponse = Either<
-  null,
+  WrongCredentialsError,
   {
     accessToken: string
   }
@@ -32,9 +31,20 @@ export class AuthenticateUserUseCase {
   }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
     const user = await this.usersRepository.findByEmail(email)
     if (!user) {
-      return left(new UserAlreadyExistError(email))
+      return left(new WrongCredentialsError())
+    }
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      user.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
     }
 
-    return right({ user })
+    const accessToken = await this.encrypter.encrypt({
+      sub: user.id.toString(),
+    })
+    return right({ accessToken })
   }
 }
